9032200f6136df343757abebda16074f338fae38
Jorin-Post <jorin101@hotmail.nl>
2022-01-25 14:08:01 +0100

    First commit

---
+++ src/main.cpp
@@ -0,0 +1,209 @@
+#include <Arduino.h>
+#include <Wire.h> //Slave
+#include <IRremote.h>
+
+IRsend irsend;
+
+int tmpset = 18, humset = 60, lghtset = 50, rad = 4, sch = 7, R1 = 11, R2 = 10, R3 = 12, bui = 55, bin = 55, bov, lght = 55, hour = 55, min = 55, tmp = 55, hum = 55, heat = 55;
+bool ven = HIGH, win = LOW, pven = HIGH, pheat = HIGH, pwin = LOW;
+
+void setup() {
+  Serial.begin(9600);
+  Wire.begin(0x8);               
+  Wire.onReceive(receiveEvent);
+  Wire.onRequest(dataRqst);
+  pinMode(rad, INPUT);pinMode(sch, INPUT);pinMode(R1, OUTPUT);pinMode(R2, OUTPUT);pinMode(R3, OUTPUT);
+  digitalWrite(R1, LOW); digitalWrite(R2, LOW); digitalWrite(R3, LOW);
+}
+
+void receiveEvent(int howMany) {
+  while (Wire.available()) {
+    hour = Wire.read();
+    min = Wire.read();
+    tmp = Wire.read();
+    hum = Wire.read();
+    bin = Wire.read();
+    bov = Wire.read();
+  }
+}
+void dataRqst(){  
+  Wire.write(bui);
+  Wire.write(heat);
+}
+void f(){
+  Serial.write(0xff);      Serial.write(0xff);      Serial.write(0xff);
+}
+void page1(){
+  bui = (((analogRead(A0)*(5000/1024))-500)/100)+10; 
+  //lght = analogRead(A3)/ 10;
+
+  Serial.print("t0.txt=");
+  Serial.print("\""); Serial.print(hour); Serial.print("\""); f();
+  Serial.print("t1.txt=");
+  Serial.print("\""); Serial.print(min); Serial.print("\""); f();
+  Serial.print("t2.txt=");
+  Serial.print("\""); Serial.print(tmp); Serial.print("\""); f();
+  Serial.print("t3.txt=");
+  Serial.print("\""); Serial.print(bui); Serial.print("\""); f();
+  Serial.print("t4.txt=");
+  Serial.print("\""); Serial.print(bin); Serial.print("\""); f();
+  Serial.print("t5.txt=");
+  Serial.print("\""); Serial.print(bov); Serial.print("\""); f();
+  Serial.print("t6.txt=");
+  Serial.print("\""); Serial.print(hum); Serial.print("\""); f();
+  Serial.print("t7.txt=");
+  Serial.print("\""); Serial.print(lght); Serial.print("\""); f();
+}
+
+void page3(){
+  Serial.print("t0.txt=");
+  Serial.print("\""); Serial.print(tmpset); Serial.print("\""); f();
+  Serial.print("t1.txt=");
+  Serial.print("\""); Serial.print(humset); Serial.print("\""); f();
+  Serial.print("t2.txt=");
+  Serial.print("\""); Serial.print(lghtset); Serial.print("\""); f();
+  if (ven == HIGH)
+  Serial.print("b0.pic=15");  f();
+  if (ven == LOW)
+  Serial.print("b0.pic=16");  f();
+  if (heat == HIGH)
+  Serial.print("b2.pic=17");  f();
+  if (heat == LOW)
+  Serial.print("b2.pic=18");  f();
+  if (win == HIGH)
+  Serial.print("b3.pic=20");  f();
+  if (win == LOW)
+  Serial.print("b3.pic=19");  f();
+}
+
+void light(){
+  if (lght < lghtset && rad == HIGH){
+    irsend.sendNEC(0xF7C03F, 32);
+  }
+  if (sch == LOW){
+    irsend.sendNEC(0xF740BF, 32);
+  } 
+}
+
+void loop() {
+  while (Serial.available() > 0){
+    int txt = Serial.read();
+    switch (txt){
+      case 1:
+      page1();
+      break;
+      case 3:
+      page3();
+      break;
+      case 10:
+        irsend.sendNEC(0xF7C03F, 32); // on
+        break;
+      case 11:
+        irsend.sendNEC(0xF740BF, 32); // off
+        break;
+      case 12:
+        irsend.sendNEC(0xF700FF, 32); // bright
+        break;
+      case 13:
+        irsend.sendNEC(0xF7807F, 32); // dim
+        break;
+      case 14:
+        irsend.sendNEC(0xF720DF, 32); // white+
+        break;
+      case 15:
+        irsend.sendNEC(0xF7A05F, 32); // white-
+        break;
+      case 16:
+        irsend.sendNEC(0xF7609F, 32); // jellow+
+        break;
+      case 17:
+        irsend.sendNEC(0xF7E01F, 32); // jellow-
+        break;
+      case 18:
+        irsend.sendNEC(0xF7D02F, 32); // flash
+        break;
+      case 19:
+        irsend.sendNEC(0xF7F00F, 32); // fade
+        break;
+      case 20:
+        irsend.sendNEC(0xF7C837, 32); // quick
+        break;
+      case 21:
+        irsend.sendNEC(0xF7E817, 32); // slow
+        break;
+      case 30:
+      if (ven == LOW) {
+        pven = HIGH;
+      }
+      else {
+        pven = LOW;
+      }
+        break;
+      case 31:
+      if (heat == LOW) {
+        pheat = HIGH;
+      }
+      else {
+        pheat = LOW;
+      }
+        break;  
+      case 32:
+      if (pwin == LOW) {
+        win = HIGH;
+        pwin = HIGH;
+      }
+      else {
+        win = LOW;
+      }
+        break;   
+      case 33:
+        tmpset ++;
+        break;   
+      case 34:
+        tmpset --;
+        break;    
+      case 35:
+        humset += 5;
+        break;
+      case 36:
+        humset -= 5;
+        break;
+        case 37:
+        lghtset += 50;
+        break;
+        case 38:
+        lghtset -= 50;
+        break;
+     default:
+     break;    
+    }
+  }
+  light();
+  if (pheat == HIGH){
+    heat = 0;
+  }
+  /*else if (tmp < (tmpset-1) && pheat == LOW){
+    heat = HIGH;
+  }
+  else if (tmp > (tmpset+1) && pheat == LOW) {
+    heat = LOW;
+  }*/
+  else {
+    heat = 100;
+  }
+  digitalWrite(R1, heat);
+  
+  if (pven == HIGH){
+    ven = HIGH;
+  }
+  /*else if (hum > (humset+10) && pven == LOW){
+    ven = HIGH;
+  }
+  else if (hum < (humset-10) && pven == LOW) {
+    ven = LOW;
+  }*/
+  else {
+    ven = LOW;
+  }
+  digitalWrite(R2, ven);
+}
